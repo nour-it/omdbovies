09cbbfa8dbcd60c44be0aad6edb23948da222cc0
nour-it <reply.nour.it@gmail.com>
2025-01-21 20:43:32 +0000

    (FEAT) : Home Page.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
     return (
         <div className='col-span-7 grid place-items-center'>
             <div className='grid place-items-center w-full h-full'>
-                <p>Movies</p>
+                <p>Movies...</p>
             </div>
         </div>
     )
